ae5b8d06c8ac6bf958f582ac7e1ad6d63c1b6ecf
MalwinTri <trimmel.m02@gmail.com>
2024-10-04 00:28:46 +0200

    added MethodUtilities for processing HTTP methods and the RequestContext class for storing HTTP request data

---
+++ HTTP/RequestContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trimmel_MCTG.HTTP
+{
+    public class RequestContext
+    {
+        public HttpMethod Method { get; set; }
+        public string ResourcePath { get; set; }
+        public string HttpVersion { get; set; }
+        public string token { get; set; }
+        public Dictionary<string, string> Header { get; set; }
+        public string? Payload { get; set; }
+
+        public RequestContext()
+        {
+            this.Method = HttpMethod.Get;
+            this.ResourcePath = "";
+            this.token = "";
+            this.HttpVersion = "HTTP/1.1";
+            this.Header = new Dictionary<string, string>();
+            this.Payload = null;
+        }
+    }
+}
