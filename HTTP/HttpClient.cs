3b152693743d4e87a70db17e47af9c6684fe1c46
MalwinTri <trimmel.m02@gmail.com>
2024-10-03 20:34:28 +0200

    Adding HttpClient and HttpServer basic config

---
+++ HTTP/HttpClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trimmel_MCTG.HTTP
+{
+    public class HttpClient
+    {
+        private readonly TcpClient ?connection;
+
+        public HttpClient(TcpClient? connection)
+        {
+            this.connection = connection;
+        }
+
+    }
+}
---
+++ HTTP/HttpServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Trimmel_MCTG.HTTP
+{
+    public class HttpServer
+    {
+        private readonly TcpListener tcpListener;
+
+        private bool listener;
+
+        public HttpServer(IPAddress address, int port)
+        {
+            tcpListener = new TcpListener(address, port);
+        }
+
+        public void Start()
+        {
+            tcpListener.Start();
+            listener = true;
+
+            while (listener)
+            {
+                var connection = tcpListener.AcceptTcpClient();
+                Console.WriteLine("Client connected");
+
+                var clientThread = new Thread(() =>
+                {
+                    var client = new Trimmel_MCTG.HTTP.HttpClient(connection);
+
+                    var request = client.ReciveRequest();
+                });
+            }
+        }
+    }
+}
